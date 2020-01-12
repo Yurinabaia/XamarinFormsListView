19331cc6705c345d7ce17098f764561096569676
Yurinabaia <yurinabaiaduarte@gmail.com>
2020-01-12 12:18:34 -0300

    Aplicação com ListView por codigo em C# e ListView por XAML

---
+++ ListView/ListView/ConfigXamarin/ListViewPage.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ListView.Modelo;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace ListView.ConfigXamarin
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class ListViewPage : ContentPage
+    {
+        public ListViewPage()
+        {
+            InitializeComponent();
+            List<Lista> obj = new List<Lista>();
+            obj.Add(new Lista{Nome = "Pedro", Sexo = "M"});
+            obj.Add(new Lista{Nome = "Joao", Sexo = "M"});
+            obj.Add(new Lista{Nome = "Larrisa", Sexo = "F"});
+            obj.Add(new Lista{Nome = "THAIS", Sexo = "F"});
+            obj.Add(new Lista{Nome = "JUAN", Sexo = "M"});
+            obj.Add(new Lista{Nome = "TETEU", Sexo = "F"});
+            LISTAVIEW.ItemsSource = obj;
+        }
+    }
+}
---
+++ ListView/ListView/Modelo/Lista.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ListView.Modelo
+{
+    class Lista
+    {
+        public string Nome { get; set; }
+        public string Sexo { get; set; }
+    }
+}
